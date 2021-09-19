ef29aaa45455f12b4a4ce497b34618fc892523bc
Mustapha Saad <mustaphasaad321@gmail.com>
2021-09-19 12:02:42 +0100

    =Set pl.1 as default

--- js/clients.js
+++ js/clients.js
@@ -10,7 +10,7 @@ function filterPlansByFloor() {
       .querySelector(".section-clients .area")
       .classList.add("select-plants");
 
-      document.querySelector(".col-plants").classList.remove("d-none");
+    document.querySelector(".col-plants").classList.remove("d-none");
 
     document.querySelectorAll(".wrapper").forEach((elemRef) => {
       elemRef.classList.remove("show");
@@ -29,13 +29,22 @@ function filterPlansByFloor() {
       .querySelector(".section-clients .area")
       .classList.remove("select-plants");
 
-      document.querySelector(".col-plants").classList.add("d-none");
-      document.querySelector(".col-building").classList.remove("opacity-0");
-
+    document.querySelector(".col-plants").classList.add("d-none");
+    document.querySelector(".col-building").classList.remove("opacity-0");
 
     document.querySelectorAll(".floors li").forEach((floorRef) => {
       floorRef.classList.remove("select");
     });
+  });
+
+  document.querySelectorAll(".floors li").forEach((floorRef) => {
+    if (floorRef.classList.contains("select")) {
+      document
+        .querySelectorAll(`.wrapper[data-anchor='${floorRef.classList.item(0)}']`)
+        .forEach((elemRef) => {
+          elemRef.classList.add("show");
+        });
+    }
   });
 }
 
